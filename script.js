4537833fbd0ad37777bc2d8319d16a7fa38bee2a
Vengadesh <145902761+VengadeshProgrammer@users.noreply.github.com>
2025-10-27 05:27:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Supabase configuration
 const SUPABASE_URL = 'https://cvqlhrrqlfddyxndcoag.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2cWxocnJxbGZkZHl4bmRjb2FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1MTU2MzcsImV4cCI6MjA3NzA5MTYzN30.i1y6_RvqV-Qc4881f_sIit9rLp7-hfrzSm_T9n9ogUU';
